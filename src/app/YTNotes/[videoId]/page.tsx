cd0e0313f57ca125c3ea9bac009a9918c88dab3f
KOSANG YADAV <kosangyadav2004@gmail.com>
2025-02-08 20:58:53 +0530

    fetch caption in YTNotes component

--- src/app/YTNotes/[videoId]/page.tsx
+++ src/app/YTNotes/[videoId]/page.tsx
@@ -1,5 +1,8 @@
-import {notes_prompt} from "@/prompts/prompts"
-import React from "react";
+import { fetchCaptions } from '@/lib/captions';
+import { generateContent } from '@/lib/gemini';
+import { notes_prompt } from '@/prompts/prompts';
+import { useParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 type Note = {
   start_time: string;
@@ -11,7 +14,33 @@ type NotesProps = {
   notes: Note[];
 };
 
-const NotesList: React.FC<NotesProps> = ({ notes }) => {
+const NotesList: React.FC<NotesProps> = async () => {
+  const [captions, setCaptions] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+  console.log(params);
+  const videoId = params.videoId;
+
+  useEffect(() => {
+    fetchCaptions(videoId).then((captions) => {
+      setCaptions(captions);
+      setLoading(false);
+    });
+  }, []);
+
+  if (!loading) console.log(captions);
+
+  const jsonString = JSON.stringify(captions);
+
+  const prompt = `${notes_prompt}: ${jsonString}`;
+
+  const content = await generateContent(prompt);
+
+  const parsedContent = JSON.parse(content.response.text());
+
+  console.log('content:', parsedContent);
+
   return (
     <div className="p-6 bg-gray-100 rounded-lg shadow-md">
       <h2 className="text-xl font-semibold mb-4">Extracted Notes</h2>
